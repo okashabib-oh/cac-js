fe266865f910e8e74c5eda205ed70f5de3889da1
Okasha Habib <160734391+okashabib-oh@users.noreply.github.com>
2024-08-05 22:27:25 +0000

    started learning

---
+++ 01/test.js
@@ -0,0 +1,16 @@
+const userId = 19071
+let userName = "Okasha Habib"
+var userEmail = "okasha@gmail.com"
+let userCity = "Karachi"
+let userState;
+let isPaid;
+userState = "Sindh"
+
+console.table([userId, userName, userEmail, userCity, userState, isPaid])
+console.log(userId, userName, userEmail, userCity, userState, isPaid)
+console.group(userId, userName, userEmail, userCity, userState, isPaid)
+console.info(userId, userName, userEmail, userCity, userState, isPaid)
+console.time(userId, userName, userEmail, userCity, userState, isPaid)
+console.count(userId, userName, userEmail, userCity, userState, isPaid)
+console.dirxml(userId, userName, userEmail, userCity, userState, isPaid)
+console.trace(userId, userName, userEmail, userCity, userState, isPaid)
